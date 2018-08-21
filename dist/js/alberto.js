3241a61c920b452353e5b573340dd9dd77fbad33
toroalbert <toroalbert@gmail.com>
2018-08-20 23:18:58 -0400

    Update 20/08/2018
    Creando archivo alberto.js

---
+++ dist/js/alberto.js
@@ -0,0 +1,213 @@
+$(document).ready(function () {
+    $('#update-skin').on('click', function (evt) {
+        $.ajax({
+            async: true,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/x-www-form-urlencoded",
+            url: "./updateskin",
+            data: "skin=" + $("#new-skin").val(),
+            beforeSend: function (datos) {
+                $('#update-skin').attr('disabled', 'true');
+                $('#send').html('<i class="fa fa-spinner fa-spin fa-fw"></i> Actualizando..');
+            },
+            success: function (datos) {
+                console.log(datos);
+                $('#update-skin').removeAttr('disabled');
+                $('#send').html('<i class="fa fa-check"></i> Actualizar');
+                $('.answerstyle').html(datos.answer);
+            },
+            error: function (datos) {
+                $('#update-skin').removeAttr('disabled');
+                $('#send').html('<i class="fa fa-warning"></i> Actualizar');
+                $('.answerstyle').html('<code>' + datos.responseText + '</code>');
+                console.log(datos);
+            }
+        });
+    });
+    $('.change-skin').on('click', function (evt) {
+        var dataskin = $(this).attr('data-skin')
+        var boxskin = $(this).attr('box-skin')
+        evt.preventDefault();
+        // console.log(boxskin);
+        $("body").removeClass();
+        $("body").addClass(dataskin);
+        $("body").addClass('sidebar-mini');
+        $("#new-skin").val(dataskin);
+    });
+    $('#change-psw').on('submit', function (evt) {
+        evt.preventDefault();
+        ruta = "./changepass"
+        data = $(this).serialize()
+        $.ajax({
+            async: true,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/x-www-form-urlencoded",
+            url: ruta,
+            data: data,
+            beforeSend: function (datos) {
+                $('.send').attr('disabled', 'true');
+                $('.send').html('<i class="fa fa-spinner fa-spin fa-fw"></i> Cambiando..');
+            },
+            success: function (datos) {
+                $('.send').html('Cambiar');
+                if (datos.success) {
+                    $('.help').removeClass('text-danger');
+                    $('.help').addClass('text-success');
+                } else {
+                    $('.help').addClass('text-danger');
+                    $('.help').removeClass('text-success');
+                }
+                $('.help').html(datos.answer);
+            },
+            error: function (datos) {
+                $('.send').html('Cambiar');
+                console.log("Error");
+                console.log(datos);
+            }
+        });
+    });
+    $('.pass').keyup(function () {
+        // console.log($('.password').val());
+        // console.log($('.re-password').val());
+        if ($('.password').val() != '' && $('.password').val().length > 5) {
+            if ($('.password').val() == $('.re-password').val()) {
+                $('.help').removeClass('text-danger');
+                $('.help').addClass('text-success');
+                $('.re-icon-pass').css('color', '#3c763d');
+                $('.help').html('Contraseñas coinciden');
+                $('.re-icon-pass').removeClass('glyphicon-lock');
+                $('.re-icon-pass').removeClass('glyphicon-remove');
+                $('.re-icon-pass').addClass('glyphicon-ok');
+
+                $('.send').removeAttr('disabled');
+            } else {
+                $('.help').removeClass('text-success');
+                $('.help').addClass('text-danger');
+                $('.re-icon-pass').css('color', '#a94442');
+                $('.help').html('Contraseñas no coinciden');
+                $('.re-icon-pass').removeClass('glyphicon-lock');
+                $('.re-icon-pass').removeClass('glyphicon-ok');
+                $('.re-icon-pass').addClass('glyphicon-remove');
+                $('.send').attr('disabled', 'true');
+            }
+        } else {
+            $('.help').removeClass('text-success');
+            $('.help').addClass('text-danger');
+            $('.help').html('Contraseña de 5 caracteres o mas');
+            $('send').attr('disabled', 'true');
+        }
+    });
+});
+function test() {
+    var ruta = './api/' + $('#urlapi').val();
+    var username = $('#userapi').val();
+    var password = $('#passwordapi').val();
+    var datos = $('#datosapi').val();
+    $('.answer').html('Cargando...');
+    $.ajax({
+        async: true,
+        type: "POST",
+        // dataType: "json",
+        dataType: "html",
+        contentType: "application/x-www-form-urlencoded",
+        headers: {
+            "Authorization": "Basic " + btoa(username + ":" + password),
+        },
+        url: ruta,
+        data: datos,
+        error: function (data) {
+            // console.log('error');
+            // console.log(data);
+            $('.answer').html(data.responseText);
+        },
+        success: function (data) {
+            // console.log('success');
+            // console.log(data);
+            $('.answer').html(data);
+        },
+    });
+}
+function accessapi(change) {
+    if (change) {
+        var ruta = "./accessapi/newpw";
+    } else {
+        var ruta = "./accessapi";
+    }
+    $.ajax({
+        async: true,
+        type: "POST",
+        dataType: "json",
+        contentType: "application/x-www-form-urlencoded",
+        url: ruta,
+        data: "s=2",
+        success: function (datos) {
+            if (datos.success) {
+                $('#usapi').html(datos.answer.user);
+                $('#pwapi').html(datos.answer.password);
+            } else {
+                alert(datos.answer);
+            }
+        }
+    });
+}
+$(document).ready(function () {
+    accessapi();
+    // console.log($("#test-api"));
+    // console.log($("#test-api")[0]);
+    var prevsrc = $('#chng-profile').attr('src');;
+
+    $("#test-api").on("submit", function () {
+
+        var tempm = $('#datosapi').val().split("&");
+        $('.temp-camp').html('');
+        for (i in tempm) {
+            var temp = tempm[i].split('=');
+            if (temp[0] == 'access_token') {
+                temp[1] += '==';
+            }
+            var html = '<input type="hidden" class="form-control" value="' + temp[1] + '" name="' + temp[0] + '" placeholder="' + temp[0] + '">';
+            $('.temp-camp').append(html);
+        }
+        var ruta = './api/' + $('#urlapi').val();
+        var username = $('#userapi').val();
+        var password = $('#passwordapi').val();
+        var formData = new FormData($("#test-api")[0]);
+        console.log(formData);
+        $('.answer').html('Cargando...');
+        $.ajax({
+            url: ruta,
+            type: "POST",
+            dataType: "html",
+            headers: {
+                "Authorization": "Basic " + btoa(username + ":" + password),
+            },
+            data: formData,
+            contentType: false,
+            processData: false,
+            error: function (data) {
+                console.log('error');
+                console.log(data);
+                $('.answer').html(data.responseText);
+            },
+            success: function (data) {
+                console.log('success');
+                console.log(data);
+                $('.answer').html(data);
+            },
+        });
+        return false;
+    });
+});
+function justNumbers(e) {
+    var keynum = window.event ? window.event.keyCode : e.which;
+    if ((keynum == 8) || (keynum == 46))
+        return true;
+    return /\d/.test(String.fromCharCode(keynum));
+}
+function clearjQueryCache() {
+    for (var x in jQuery.cache) {
+        delete jQuery.cache[x];
+    }
+}
