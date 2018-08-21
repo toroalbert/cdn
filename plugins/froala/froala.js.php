97d60af8ca40dbfdde559b91840c450978f17729
toroalbert <toroalbert@gmail.com>
2018-08-20 23:21:41 -0400

    Change Folder
    cambiando directorio a area

--- plugins/froala/froala.js.php
+++ plugins/froala/froala.js.php
@@ -1,36 +1,36 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js"></script>
   
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/froala_editor.min.js" ></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/align.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/char_counter.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/code_beautifier.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/code_view.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/colors.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/draggable.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/emoticons.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/entities.min.js"></script>
-  <!-- <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/file.min.js"></script> -->
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/font_size.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/font_family.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/fullscreen.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/image.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/image_manager.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/line_breaker.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/inline_style.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/link.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/lists.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/paragraph_format.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/paragraph_style.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/quick_insert.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/quote.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/table.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/save.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/url.min.js"></script>
-  <!-- <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/video.min.js"></script> -->
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/help.min.js"></script>
-  <!-- <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/print.min.js"></script> -->
-  <!-- <script type="text/javascript" src="./adminlte/plugins/froala/js/third_party/spell_checker.min.js"></script> -->
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/special_characters.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/plugins/word_paste.min.js"></script>
-  <script type="text/javascript" src="./adminlte/plugins/froala/js/languages/es.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/froala_editor.min.js" ></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/align.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/char_counter.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/code_beautifier.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/code_view.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/colors.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/draggable.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/emoticons.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/entities.min.js"></script>
+  <!-- <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/file.min.js"></script> -->
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/font_size.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/font_family.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/fullscreen.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/image.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/image_manager.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/line_breaker.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/inline_style.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/link.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/lists.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/paragraph_format.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/paragraph_style.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/quick_insert.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/quote.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/table.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/save.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/url.min.js"></script>
+  <!-- <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/video.min.js"></script> -->
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/help.min.js"></script>
+  <!-- <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/print.min.js"></script> -->
+  <!-- <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/third_party/spell_checker.min.js"></script> -->
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/special_characters.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/plugins/word_paste.min.js"></script>
+  <script type="text/javascript" src="<?= $this->url ?>/plugins/froala/js/languages/es.js"></script>
